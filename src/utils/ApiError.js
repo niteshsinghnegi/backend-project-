e079dbe0444b2851c8908ea637c50e74f3e17853
niteshsinghnegi <2007.niteshnegi@gmail.com>
2026-07-17 12:16:09 +0530

    error solve

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -3,7 +3,7 @@ class ApiError extends Error {
         statusCode,
         message="somenthing went wrong ",
         errors = [],
-        statck= ""
+        stack= ""
     ){
         super(message)
         this.statusCode=statusCode,
@@ -14,8 +14,8 @@ class ApiError extends Error {
     }
 }
 
-if (statck){
-    this.statck= statck
+if (stack){
+    this.stack= stack
 }else{
     Error.captureStackTrace(this,this.constructor)
 }
