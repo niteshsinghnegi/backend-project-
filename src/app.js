19dde68386289a5286302909277b50be29a27aea
niteshsinghnegi <2007.niteshnegi@gmail.com>
2026-07-20 22:47:51 +0530

    update bakend with controllers

--- src/app.js
+++ src/app.js
@@ -16,4 +16,18 @@ app.use (express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended : true, limit : "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+
+// routes  import 
+
+import userRouter from "./routes/user.routes.js"
+
+// routes declation 
+
+app.use("/api/v1/users",userRouter)
+// http : //localhost:8000/api/vi/users/register
+
+
+
+
+
 export { app };
